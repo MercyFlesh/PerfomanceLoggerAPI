e8a02066e852d9353d2142ab6db064fe37446c4d
Artem Rychenkov <fleshmercy@gmail.com>
2023-02-04 18:19:05 +0300

    Create structure project

---
+++ PerfomanceLogger.Api/Controllers/PerfomanceLoggerController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PerfomanceLogger.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PerfomanceLoggerController : ControllerBase
+    {
+        private readonly ILogger<PerfomanceLoggerController> _logger;
+
+        public PerfomanceLoggerController(ILogger<PerfomanceLoggerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult UploadData(IFormFile file)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public IActionResult GetFilteredData()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public IActionResult GetFileValues()
+        {
+            return Ok();
+        }
+    }
+}
