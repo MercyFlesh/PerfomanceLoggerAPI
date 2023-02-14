278d781bd8513dd377bdc95b956eacc88b87cf7d
Artem Rychenkov <fleshmercy@gmail.com>
2023-02-14 21:33:26 +0300

    Fix UpdateResultValues

--- PerfomanceLogger.Api/Services/DocumentService.cs
+++ PerfomanceLogger.Api/Services/DocumentService.cs
@@ -5,15 +5,11 @@ namespace PerfomanceLogger.Api.Services
 {
     public class DocumentService : IDocumentService
     {
-        private readonly ILogger _logger;
         private readonly IPerfomanceRepository _perfomanceRepository;
 
-        public DocumentService(
-            IPerfomanceRepository perfomanceRepository,
-            ILogger<DocumentService> logger)
+        public DocumentService(IPerfomanceRepository perfomanceRepository)
         {
             _perfomanceRepository = perfomanceRepository;
-            _logger = logger;
         }
 
         public async Task UploadCsv(Stream stream, string fileName)
@@ -47,7 +43,7 @@ namespace PerfomanceLogger.Api.Services
                 result.MeanExecutionTime = records.Average(x => x.Time);
                 result.MeanMark = records.Average(x => x.Mark);
 
-                var recordsOrderedByMark = records.OrderBy(x => x);
+                var recordsOrderedByMark = records.OrderBy(x => x.Mark);
                 if (countRecords % 2 == 0)
                     result.MedianMark = recordsOrderedByMark.Skip((countRecords / 2) - 1).Take(2).Average(x => x.Mark);
                 else
@@ -76,7 +72,7 @@ namespace PerfomanceLogger.Api.Services
             }
             else
             {
-                throw new ArgumentException($"Incorrect date field in record {csvRow}");
+                throw new ArgumentException($"Incorrect date field in record: {csvRow}");
             }
 
             if (int.TryParse(fields[1], out int time) && time >= 0)
@@ -85,7 +81,7 @@ namespace PerfomanceLogger.Api.Services
             }
             else
             {
-                throw new ArgumentException($"Incorrect time field in record {csvRow}");
+                throw new ArgumentException($"Incorrect time field in record: {csvRow}");
             }
 
             if (double.TryParse(fields[2], out double mark) && mark >= 0)
@@ -94,7 +90,7 @@ namespace PerfomanceLogger.Api.Services
             }
             else
             {
-                throw new ArgumentException($"Incorrect mark field in record {csvRow}");
+                throw new ArgumentException($"Incorrect mark field in record: {csvRow}");
             }
 
             return val;
